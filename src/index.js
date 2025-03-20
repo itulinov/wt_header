3057d88fbbbf5073dea8502d43d7433cc978b453
iTulinov <energiay@gmail.com>
2025-03-20 13:11:16 +0300

    add html&css

---
+++ src/index.js
@@ -0,0 +1,36 @@
+<style>
+    .logo-container {
+        height: 72px;
+    }
+
+    .logo-container > div {
+        display: flex;
+        height: 100%;
+    }
+    
+    .logo-container > div > a > .logo_container__text {
+        opacity: .7;
+        padding-right: 14px;
+    }
+    
+    .logo-container > div > .logo_container__link {
+        font-weight: 500;
+        font-size: 24px;
+        color: #191c34;
+        display: flex;
+        align-items: center;
+    }
+</style>
+
+
+<div id="wt-header-logo" class="logo-container">
+    <div>
+        <a href="/home" class="logo_container__link">
+            <span class="logo_container__text">корпоративный портал</span>
+            <img class="logo_container__logo"
+                src="design/default/images/logo1.png"
+            />
+            <span class="logo_container__name">WebsoftHCM</span>
+        </a>
+    </div>
+</div>
